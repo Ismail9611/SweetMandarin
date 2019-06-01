0532b294a69ad3acaf018d42d624177427fdd068
Iscool3301 <nakkazulav@mail.ru>
2019-06-02 02:18:48 +0400

    Some bugs were fixed, some were added with new features

---
+++ src/main/resources/static/js/note.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Исмаил on 29.05.2019.
+ */
+// $(document).ready(function(){
+//     var noteId = $('.note_id').text();
+//     setTimeout(function () {
+//         $.ajax({
+//             type: 'get',
+//             url: '/note/update_view_count',
+//             data: {'id': noteId},
+//             success: function (data) {
+//                 console.log(JSON.stringify(data));
+//             },
+//             error: function (err) {
+//                 console.log(err);
+//             }
+//         })
+//     }, 5000);
+//
+// });
